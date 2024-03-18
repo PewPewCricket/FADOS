e9b3ed71b49738e14783a942f8fcc26595a7a3e0
PewPewCricket <PewPewCricket@proton.me>
2024-03-17 21:15:10 -0500

    fix stage 1

--- src/boot/stage2/setup.s
+++ src/boot/stage2/setup.s
@@ -2,7 +2,34 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
-cli
+mov si, find
+call print
+cli 
 hlt
 
-find: db 'hewwo', ENDL, 0
+; print text to screen
+; ds:si = pointer to string to print
+
+print:
+    push si                             ; save si to stack
+    push ax                             ; save ax to stack
+    push bx                             ; save bx to stack
+
+.loop:
+    lodsb                               ; loads next character in al
+    or al, al                           ; verify if next character is null
+    jz .done                            ; if next character is null then jump to .done
+
+    mov ah, 0x0E                        ; call bios interrupt
+    mov bh, 0                           ; set page number to 0
+    int 0x10                            ; call interupt 0x10
+
+    jmp .loop                           ; loop printing until all characters have been printed
+
+.done:
+    pop bx                              ; restore bx from stack
+    pop ax                              ; restore ax from stack
+    pop si                              ; restore si from stack
+    ret                                 ; return from function
+
+find: db 'haiiiii :3', ENDL, 0
