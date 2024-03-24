645233b5f16fec3f888f38ec235f8072ffc82d72
PewPewCricket <PewPewCricket@proton.me>
2024-03-24 10:48:51 -0500

    update structure

--- src/boot/stage2/setup.s
+++ src/boot/stage2/setup.s
@@ -2,10 +2,138 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
-mov si, find
-call print
-cli 
+; data
+
+a20_ok:     db 'ENABLED A20'
+
+; code
+
+call enable_a20
+cli
 hlt
+
+; A20 enable
+test_a20:
+    pusha
+
+    mov ax, [0x7dfe]
+
+    push bx
+    mov bx, 0xffff
+    mov es, bx
+    pop bx
+
+    mov bx, 0x7e0e
+
+    mov dx, [es:bx]
+
+    cmp ax, dx
+    je .cont
+
+    popa
+    mov ax, 1
+    ret
+
+    .cont:
+        mov ax, [0x7dff]
+
+        push bx
+        mov bx, 0xffff
+        mov es, bx
+        pop bx
+
+        mov bx, 0x7e0f
+        mov dx, [es:bx]
+
+        cmp ax, dx
+        je .exit
+
+        popa
+        mov ax, 1
+        ret
+
+    .exit:
+        popa
+        xor ax, ax
+        ret
+
+enable_a20:
+    pusha
+
+    ;BIOS
+    mov ax, 0x2401 
+    int 0x15
+
+    call test_a20
+    cmp ax, 1
+    je .done
+
+    ;Keyboard
+    sti
+
+    call wait_c
+    mov al, 0xad
+    out 0x64, al
+
+    call wait_c
+    mov al, 0xd0
+    out 0x64, al
+
+    call wait_d 
+    in al, 0x60
+    push ax
+
+    call wait_d
+    mov al, 0xd1
+    out 0x64, al
+
+    call wait_c
+    pop ax
+    or al, 2
+    out 0x60, al
+
+    call wait_c
+    mov al, 0xae
+    out 0x64, al
+
+    call wait_c
+
+    sti
+
+    call test_a20
+    cmp ax, 1
+    je .done
+
+    ;FastA20
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    call test_a20
+    cmp al, 1
+    je .done
+
+    jmp $
+
+    .done:
+        popa
+        mov si, a20_ok
+        call print
+        ret
+
+wait_c:
+    in al, 0x64
+    test al, 2
+
+    jnz wait_c
+    ret
+
+wait_d:
+    in al, 0x64
+    test al, 1
+
+    jz wait_d
+    ret
 
 ; print text to screen
 ; ds:si = pointer to string to print
@@ -31,5 +159,3 @@ print:
     pop ax                              ; restore ax from stack
     pop si                              ; restore si from stack
     ret                                 ; return from function
-
-find: db 'Hello Stage 2!', ENDL, 0
